c9c4a0de005b811745e63d87229830da1c51d3a6
Sebastian-Aparicio <yscaysca16@hotmail.com>
2025-10-03 10:08:31 -0500

    mover imágenes a public

--- src/Componentes/SingleItem.jsx
+++ src/Componentes/SingleItem.jsx
@@ -19,14 +19,18 @@ const SingleItem = ({ price, type, id, eliminarItem, editItem }) => {
 
     }
 
-    const urlImage = `/src/images/${type}.png`
+  const urlImage = `/images/${type}.png`;
 
-    return ( <div className="single-item">
-        <img src={ urlImage } alt="services" />
-        <h3>Precio:{moneyFormat(Number(price))}</h3>
-        <a href="" className="delete" onClick={HandleDelete}>Borrar</a>
-        <a href="" className="edit" onClick={HandleEdit}>Editar</a>
-    </div> );
+return (
+  <div className="single-item">
+    <img src={urlImage} alt={type} />
+    <h3>Precio: {moneyFormat(Number(price))}</h3>
+    <a href="" className="delete" onClick={HandleDelete}>Borrar</a>
+    <a href="" className="edit" onClick={HandleEdit}>Editar</a>
+  </div>
+);
+
+
 }
  
 export default SingleItem;
